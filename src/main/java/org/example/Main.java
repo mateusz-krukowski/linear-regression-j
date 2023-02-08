58b8cd54bd2933a6d3a69856816b3324f643f891
thorlaksson <matwoz2001@icloud.com>
2023-02-08 06:13:48 +0100

    fixed reversed x and y, added different dataset for testing I guess it's working fine but idk tho

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,13 +14,15 @@ import static org.math.plot.utils.Array.min;
 /** @author Mateusz Stanisław Krukowski **/
 /*Linear Regression counting program */
 public class Main {
-    private static final int n = 128;
+    private static final int n = 128; //length
     public static double[] x = new double[n];
     public static double[] y = new double[n];
     public static double x̄ = 0; //mean value of x
     public static double ȳ = 0; // mean value of y
-    public static double a = 0;
-    public static double b = 0;
+    public static double a = 0; // slope / współczynnik kierunkowy
+    public static double b = 0; // wyraz wolny
+    public static double R = 0; // współczynnik korelacji liniowej Pearsona
+    public static double R2 = 0; //współczynnik determinacji liniowej
     public static void readFromCsv() throws IOException {
 
         InputStream inputStream = Main.class.getResourceAsStream("data.csv");
@@ -30,9 +32,10 @@ public class Main {
         int i = 0;
 
         while((line = reader.readLine()) !=null) {
+            System.out.println(line);
             String[] columns = line.split(",");
-            x[i] = Double.parseDouble(columns[1]);
-            y[i] = i+1; //Double.parseDouble(columns[1]);
+            x[i] = i+1;
+            y[i] = Double.parseDouble(columns[1]);
             i++;
         }
 
@@ -45,7 +48,7 @@ public class Main {
                 .forEach(i -> System.out.println(String.format("%.2f", x[i]) + (x[i]>=100? "   " : "    ") + y[i]));
         //if x[i] has 3 digits then 3 spaces, if it has 2 digits then 4 spaces so the space between x and y is constant
     }
-    public static double[] regresjaLiniowa(double[] x, double[] y) {
+    public static void regresjaLiniowa(double[] x, double[] y) {
 
         for (double i : x) x̄ += i;
         x̄ /= n;
@@ -57,6 +60,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             xTrans[i] = x[i] - x̄; //dla kazdego x liczymy x - sredniego x
             yTrans[i] = y[i] - ȳ; //dla kazdego y liczymy y - sredniego y
+            System.out.println(xTrans[i] + "," + yTrans[i]);
         }
 
         double Sxy = 0;
@@ -68,20 +72,15 @@ public class Main {
             Sy2 += yTrans[i] * yTrans[i];
         }
 
-        double a = Sxy / Sx2; //suma xy / suma x^2
-        double b = ȳ - a * x̄; //sredni y - ax
-        double R = Sxy / (Math.sqrt(Sx2) * Math.sqrt(Sy2));
-        double R2 = R * R;
+        a = Sxy / Sx2; //suma xy / suma x^2
+        b = ȳ - a * x̄; //sredni y - ax
+        R = Sxy / (Math.sqrt(Sx2) * Math.sqrt(Sy2));
+        R2 = R * R;
 
-        return new double[] {a, b, R, R2};
+
     }
 
     public static void doTheRegression(double[] x, double[] y, String nazwa_x, String nazwa_y) {
-        double[] res = regresjaLiniowa(x, y);
-        double a = res[0];
-        double b = res[1];
-        double R = res[2];
-        double R2 = res[3];
 
         // współrzędne x początku i końca linii regr.
         double x1 = min(x);
@@ -92,11 +91,13 @@ public class Main {
         double y2 = a * x2 + b;
 
 
+
         Plot2DPanel plot = new Plot2DPanel();
-        plot.addLinePlot("nwm",y,x);
-        plot.addScatterPlot("Scatter", y, x);
+        plot.addLinePlot("nwm",x,y);
+        plot.addScatterPlot("Scatter", x, y);
         plot.changePlotColor(0,new Color(0,0,120));
 
+        plot.addLinePlot("", Color.BLACK,  new double[] {x1,y1}, new double[]{x2,y2});
 
 
         JFrame frame = new JFrame("Linear Regression");
@@ -109,10 +110,10 @@ public class Main {
 
     }
 
-    public static void main(String[] args) throws NullPointerException, IOException, URISyntaxException {
+    public static void main(String[] args) throws IOException {
         readFromCsv();
-        debugDataUtility();
-
+        regresjaLiniowa(x,y);
+        //debugDataUtility();
         doTheRegression(x, y, "dni", "cena akcji");
     }
 }
